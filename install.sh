ecdb85818bd48e7f12757bb511afe0d46fb53030
Armin Shaker <57183836+arminhere@users.noreply.github.com>
2022-06-06 16:04:16 +0430

    Update install.sh
    added pip

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ pkg upgrade -y
 pkg install git python -y
 git clone https://github.com./arminhere/MyScripts
 cd MyScripts
+pip install requests
 echo Use python sms.py to execute the sms bomb. cntrl+C to stop
